51af8c7177b5493ce398013c751000547321122e
wwatson30 <walkerw18@outlook.com>
2025-04-20 21:59:40 -0400

    analysis framework done, bin tweaking needed to improve results

--- evaluate_results.py
+++ evaluate_results.py
@@ -2,27 +2,39 @@ import pandas as pd
 import datetime
 import transition_fn
 import get_state
+import get_target
 
 start = datetime.datetime(2021, 9, 28)
-end = datetime.datetime(2021, 12, 1)
+end = datetime.datetime(2021, 11, 31)
 date_list = pd.date_range(start, end).to_list()
 year_horizon = datetime.datetime(2015, 1, 1)
 # TODO: Change interaction between transition_fn and get_state so the current state
 #  can be generated with less repetition
 df = pd.read_csv('/Users/walkerwatson/PycharmProjects/markovian-stock-modeling/data/inflation_adjusted_berkshire_stocks.csv')
+result = pd.DataFrame(columns=['date', 'cur_state', 'predicted', 'prob', 'actual', 'success'])
 for today in date_list:
     t_func = transition_fn.transition_fn_by_prob(today=today)
     df['Date'] = pd.to_datetime(df['Date'])
-    df = df[(year_horizon < df['Date']) & (df['Date'] <= today)]
+    df = df[(year_horizon < df['Date'])]
     col = 'Open_adjusted'
     # print('pre-data_series: ' + str(time.time()))
     today_index = df[df['Date'] == today].index
     if today_index.empty:
+        print('date is empty!')
         continue
-    data_list = df[col][today_index[0] - 3: today_index[0]]
-    data_series = pd.Series(data_list)
+    pos = df.index.get_loc(today_index[0])
+    data_series = df[col].iloc[pos - 3: pos + 1]
+
     # This part is repeated from transition_fn, which is bad
-    print(data_series)
     today_state = transition_fn.Combination(get_state.get_state_by_perc_change
                                  (data_series=data_series, state_width=1), max_delta=8).get_combination()
     print(today_state)
+    tom_data_series = df[col].iloc[pos - 2: pos + 2]
+    tomorrow_state = transition_fn.Combination(get_state.get_state_by_perc_change
+                                 (data_series=tom_data_series, state_width=1), max_delta=8).get_combination()
+    print(tomorrow_state)
+    predicted, prob = get_target.get_target(t_table=t_func, cur_state=today_state, days_into_future=1)
+
+    result.loc[len(result)] = [today, today_state, predicted, prob, tomorrow_state, int(predicted == tomorrow_state)]
+
+result.to_csv('result_sheet.csv')
--- get_target.py
+++ get_target.py
@@ -5,7 +5,7 @@ import datetime
 import json
 
 
-def get_target(t_table: str = 'transition_table.json',
+def get_target(t_table: pd.DataFrame,
                cur_state=(3, 2, 1), days_into_future: int = 1):
     """
     This function takes a transition table t_table and calculates the most probable state to occur in
@@ -13,16 +13,16 @@ def get_target(t_table: str = 'transition_table.json',
     returns (result_state, result_prob)
     """
     # TODO: validate behavior for multiple transitions in future (make sure probs sum to 1)
-
-    df = pd.read_json(t_table)
-    print(df)
-    matrix = df.fillna(0)
+    cur_state = tuple(int(x) for x in cur_state)
+    matrix = t_table.fillna(0)
     power_matrix = matrix ** days_into_future  # calculate P matrix for multiple transitions in the future
-    prob_list = power_matrix[cur_state]
+    # use additional parentheses and comma to force composite index recognition
+    print(type(power_matrix.index))
+    print(cur_state in power_matrix.index)
+    prob_list = power_matrix.loc[[cur_state]].squeeze()
     prob_list = prob_list.sort_values(ascending=False)
     result_state = prob_list.index[0]
     result_prob = prob_list.iloc[0]
     return result_state, result_prob
 
 
-print(get_target(days_into_future=1))
