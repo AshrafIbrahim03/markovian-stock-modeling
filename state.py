6cabf0da83ef41473f993f517129c5e72ae8a1a3
AshrafIbrahim03 <btd2eb@virginia.edu>
2025-04-22 11:25:29 -0400

    Added StateWindow class

--- state.py
+++ state.py
@@ -1,5 +1,5 @@
 
-class State:
+class StateWindow:
     """
         This class represents a state in a Markov Chain. It abstracts a tuple of integers. The most recent state is the last element in a tuple
     """
@@ -8,13 +8,15 @@ class State:
         self.state_window = state_win
 
     def __eq__(self, value) -> bool:
-        assert type(value) == State
+        assert type(value) == StateWindow
         assert len(self.state_window) == len(value.state_window)
 
         for (s1,s2) in zip(self.state_window,value.state_window):
             if s1 is not s2:
                 return False
         return True
+    def as_tuple(self) -> tuple[int]:
+        return self.state_window
     
     def __str__(self):
         return "["+ " ".join(self.state_window) +"]"
